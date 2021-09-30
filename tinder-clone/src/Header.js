27d896d413f10330fe150889299424acf2101a5b
Joan Wilson <joanwmoliveira@gmail.com>
2021-09-30 20:02:34 -0300

    Tentando resolver o MaterialIcon

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./Header.css";
-
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 
 function Header() {
     return (
         <div className='header'>
             <h1>Header aqui</h1>
+            <AccessibilityNewIcon/>
         </div>
     )
 }
--- tinder-clone/src/index.js
+++ tinder-clone/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from "./Header.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Header />
+  </div>,
   document.getElementById('root')
 );
 
