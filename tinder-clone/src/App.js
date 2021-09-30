5ec889770101c217a3bb427aa3b75723c1c7d383
Joan Wilson <joanwmoliveira@gmail.com>
2021-09-30 13:20:17 -0300

    Tentando Resolver erro do Material UI

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import './App.css';
-import Header from "./Header";
-
+import Header from "./Header.js";
 
 function App() {
   return (
     <div className="app">
-
+      
       <Header />
       {/*Cartões do Tinder*/}
       {/*Botões de passar */}
