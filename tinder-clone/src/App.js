dc8e986a8e17ad45c97d55300115a673b5ac837e
Joan Wilson <joanwmoliveira@gmail.com>
2021-09-30 11:03:34 -0300

    Instalando alguns pacotes e testando aplicação

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Header from "./Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Eu creio que irei conseguir criar esse APP MERN tinder clone
-      </h1>
+    <div className="app">
+
+      <Header />
+      {/*Cartões do Tinder*/}
+      {/*Botões de passar */}
+
     </div>
   );
 }
