ad40c63d102229a16511699da61b5ea31fed5cc7
Joan Wilson <joanwmoliveira@gmail.com>
2021-09-30 00:52:18 -0300

    Building the project

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Eu creio que irei conseguir criar esse APP MERN tinder clone
+      </h1>
+    </div>
+  );
+}
+
+export default App;
